85bc8148ae780ab82d3291e8b9c8f43bb82c88fc
KingOfTheAce2 <84872320+KingOfTheAce2@users.noreply.github.com>
2025-06-08 14:05:40 +0200

    Update scraper to use AJAX search

--- scrape_and_push.py
+++ scrape_and_push.py
@@ -6,6 +6,7 @@ from huggingface_hub import HfApi, HfFolder, Repository, upload_file
 
 BASE_URL = "https://rechtspraak.sr"
 SEARCH_URL = f"{BASE_URL}/zoekresultaat"
+AJAX_URL = f"{BASE_URL}/wp-content/themes/understrap/templates/loadmore_search_results.php"
 DATASET_REPO_ID = "vGassen/Surinam-Dutch-Court-Cases"
 SOURCE = "Uitsprakendatabank van het Hof van Justitie van Suriname"
 
@@ -14,10 +15,47 @@ HEADERS = {
 }
 
 def get_case_links():
-    res = requests.get(SEARCH_URL, headers=HEADERS)
-    soup = BeautifulSoup(res.text, "html.parser")
-    links = soup.select("a[href*='sru-']")
-    urls = {BASE_URL + link['href'] for link in links}
+    """Retrieve case URLs by mimicking the site's AJAX search."""
+    session = requests.Session()
+
+    page = 1
+    urls = set()
+
+    while True:
+        payload = {
+            "paged": page,
+            "sText": "",
+            "sField": "",
+            "sSortOrder": "pronunciation_asc",
+            "sAuthority[]": "all",
+            "sCaseNumber": "",
+            "sJurisdiction[]": "all",
+            "declaration_date": "",
+            "publication_date": "",
+            "sDateType": "",
+            "sDatePeriod": "",
+            "sStartDate": "",
+            "sEndDate": "",
+            "search_type": "advance_search",
+            "search_type2": "advance_search",
+            "max_records_display": "200",
+            "sSruNumber": "",
+        }
+
+        res = session.post(AJAX_URL, headers=HEADERS, data=payload)
+        soup = BeautifulSoup(res.text, "html.parser")
+        links = soup.select("a[href*='sru-']")
+        if not links:
+            break
+        for link in links:
+            href = link.get("href")
+            if not href:
+                continue
+            if not href.startswith("http"):
+                href = BASE_URL + href
+            urls.add(href)
+        page += 1
+
     return list(urls)
 
 def scrape_case(url):
